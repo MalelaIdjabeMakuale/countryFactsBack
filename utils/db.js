6c0907e780beeab1e7d5fffe7c458f14e6e88bd5
bokombe <malela.im90@gmail.com>
2024-06-21 13:14:48 +0200

    cambios vercel json

--- utils/db.js
+++ utils/db.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('INFO: Conexión a base de Datos correcta');
+    conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      "INFO: Conexión a base de Datos correcta:",
+      conn.connection.name
+    );
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
+    console.log(ERROR), error.message;
   }
 };
 
