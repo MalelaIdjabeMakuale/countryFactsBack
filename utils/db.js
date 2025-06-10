3ee215744d6d2defbc017797a30b3e9079a749da
bokombe <malela.im90@gmail.com>
2025-06-10 15:17:31 +0200

    minor changes

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectMongo = async () => {
   try {
-    conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const port = process.env.CUSTOM_PORT || 3000;
     console.log(
-      "INFO: Conexión a base de Datos correcta:",
-      conn.connection.name
+      `✅ Conexión a base de datos correcta: ${conn.connection.name}\n🚀 Servidor escuchando en el puerto ${port}\n🔗 http://localhost:${port}`
     );
   } catch (error) {
-   error.message;
+    console.error("❌ Error al conectar a la base de datos:", error.message);
   }
 };
 
