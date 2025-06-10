8406cdfe26b684a4e18dc6438472b53526130cf5
bokombe <malela.im90@gmail.com>
2025-06-10 13:40:05 +0200

    cambio endpoint

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(mongoSanitize());
 
 /* ROUTES */
 
-app.use("/api/factmap", countriesRouter);
+app.use("/factmap", countriesRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({
